9b96d14743801d994925b3f7a99f7b11bdb4dcef
dspwebstudio <danielperezber@outlook.com>
2020-05-18 08:19:18 -0500

    navbar changes

--- dist/js/navbar.js
+++ dist/js/navbar.js
@@ -1,8 +1,11 @@
 $(window).resize(function() {
     var viewportWidth = $(window).width();
     if (viewportWidth < 1240) {
-            $('.nav-link').click(function () { 
+            $('.js-nav-click').click(function () { 
                 $('.navbar-toggler').click();
+            });
+            $('.navbar-brand').click(function () { 
+                $('.navbar-nav').hide();
             });
     }
 });
